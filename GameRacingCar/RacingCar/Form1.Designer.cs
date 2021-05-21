a7c252c418562364018e4119555846f67dc7dada
hoangkien2000 <1851050068kien@ou.edu.vn>
2021-05-21 09:42:35 +0700

    first commit

---
+++ GameRacingCar/RacingCar/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.mycar = new System.Windows.Forms.PictureBox();
+            this.moto1 = new System.Windows.Forms.PictureBox();
+            this.moto2 = new System.Windows.Forms.PictureBox();
+            this.moto3 = new System.Windows.Forms.PictureBox();
+            this.moto4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mycar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moto1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moto2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moto3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moto4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(225, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 95);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(225, 180);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 95);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox3.Location = new System.Drawing.Point(225, 369);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 103);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox4.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 472);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox5.Location = new System.Drawing.Point(422, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 472);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // mycar
+            // 
+            this.mycar.Image = ((System.Drawing.Image)(resources.GetObject("mycar.Image")));
+            this.mycar.Location = new System.Drawing.Point(47, 379);
+            this.mycar.Name = "mycar";
+            this.mycar.Size = new System.Drawing.Size(28, 50);
+            this.mycar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mycar.TabIndex = 5;
+            this.mycar.TabStop = false;
+            // 
+            // moto1
+            // 
+            this.moto1.Image = ((System.Drawing.Image)(resources.GetObject("moto1.Image")));
+            this.moto1.Location = new System.Drawing.Point(149, 99);
+            this.moto1.Name = "moto1";
+            this.moto1.Size = new System.Drawing.Size(28, 50);
+            this.moto1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.moto1.TabIndex = 6;
+            this.moto1.TabStop = false;
+            // 
+            // moto2
+            // 
+            this.moto2.Image = ((System.Drawing.Image)(resources.GetObject("moto2.Image")));
+            this.moto2.Location = new System.Drawing.Point(47, 45);
+            this.moto2.Name = "moto2";
+            this.moto2.Size = new System.Drawing.Size(28, 50);
+            this.moto2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.moto2.TabIndex = 7;
+            this.moto2.TabStop = false;
+            // 
+            // moto3
+            // 
+            this.moto3.Image = ((System.Drawing.Image)(resources.GetObject("moto3.Image")));
+            this.moto3.Location = new System.Drawing.Point(276, 26);
+            this.moto3.Name = "moto3";
+            this.moto3.Size = new System.Drawing.Size(28, 50);
+            this.moto3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.moto3.TabIndex = 8;
+            this.moto3.TabStop = false;
+            // 
+            // moto4
+            // 
+            this.moto4.Image = ((System.Drawing.Image)(resources.GetObject("moto4.Image")));
+            this.moto4.Location = new System.Drawing.Point(351, 148);
+            this.moto4.Name = "moto4";
+            this.moto4.Size = new System.Drawing.Size(28, 50);
+            this.moto4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.moto4.TabIndex = 9;
+            this.moto4.TabStop = false;
+            this.moto4.Click += new System.EventHandler(this.moto4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(263, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Your score:\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(360, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 23);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(434, 461);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.moto4);
+            this.Controls.Add(this.moto3);
+            this.Controls.Add(this.moto2);
+            this.Controls.Add(this.moto1);
+            this.Controls.Add(this.mycar);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mycar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moto1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moto2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moto3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moto4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox mycar;
+        private System.Windows.Forms.PictureBox moto1;
+        private System.Windows.Forms.PictureBox moto2;
+        private System.Windows.Forms.PictureBox moto3;
+        private System.Windows.Forms.PictureBox moto4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GameRacingCar/RacingCar/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            linemove(carspeed);
+            gameover();
+            totalsocre();   
+        }
+
+        Random pos = new Random();
+        void linemove(int speed)
+        {
+            if (pictureBox1.Top > 500) pictureBox1.Top = 0;
+            else pictureBox1.Top += speed;
+            if (pictureBox2.Top > 500) pictureBox2.Top = 0;
+            else pictureBox2.Top += speed;
+            if (pictureBox3.Top > 500) pictureBox3.Top = 0;
+            else pictureBox3.Top += speed;
+            if (moto1.Top > 500)
+            {
+                score++;
+                moto1.Left = pos.Next(0, 200);
+                moto1.Top = pos.Next(0, 40);
+                moto1.Top = 0;
+            }
+            else moto1.Top += speed;
+            if (moto2.Top > 500)
+            {
+                score++;
+                moto2.Left = pos.Next(0, 200);
+                moto2.Top = pos.Next(0, 40);
+                moto2.Top = 0;
+            }
+            else moto2.Top += speed;
+            if (moto3.Top > 500)
+            {
+                score++;
+                moto3.Left = pos.Next(250, 400);
+                moto3.Top = pos.Next(0, 40);
+
+                moto3.Top = 0;
+            }
+            else moto3.Top += speed;
+            if (moto4.Top > 500)
+            {
+                score++;    
+                moto4.Left = pos.Next(250, 400);
+                moto4.Top = pos.Next(0, 40);
+                moto4.Top = 0;
+            }
+            else moto4.Top += speed;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int carspeed = 2;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && mycar.Left>0) mycar.Left -= 3;
+            if (e.KeyCode == Keys.Right && mycar.Left < 400) mycar.Left += 3;
+            if (e.KeyCode == Keys.Up && carspeed < 30) carspeed++;
+            if (e.KeyCode == Keys.Down && carspeed >2) carspeed--;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int score;
+        void totalsocre()
+        {
+            textBox1.Text = score.ToString(); 
+        }
+        void gameover()
+        {
+            if(mycar.Bounds.IntersectsWith(moto1.Bounds) || mycar.Bounds.IntersectsWith(moto2.Bounds)|| mycar.Bounds.IntersectsWith(moto3.Bounds)|| mycar.Bounds.IntersectsWith(moto4.Bounds))
+            {
+                timer1.Enabled = false;
+                DialogResult go = MessageBox.Show("Do you want play again ? ", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+                switch(go)
+                {
+                    case System.Windows.Forms.DialogResult.Yes:
+                        timer1.Enabled = true;
+                        score = 0;
+                        moto1.Left = pos.Next(0, 200);
+                        moto1.Top = 0;
+                        moto2.Left = pos.Next(0, 200);
+                        moto2.Top = 0;
+                        moto3.Left = pos.Next(250, 390);
+                        moto3.Top = 0;
+                        moto4.Left = pos.Next(250, 390);
+                        moto4.Top = 0;
+                        break;
+                    case System.Windows.Forms.DialogResult.No:
+                        this.Close();
+                        break;
+
+                }
+              
+            }
+
+        }
+
+        private void moto4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
